a2e80f2f3fdf9f4c30c77531f622277c40ae6dfd
GabrielGoncalvesDeOliveira <gabrielgo15r@gmail.com>
2022-10-09 16:15:36 -0300

    Criacao da Migration inicial. Ajuste na classe Contexto para criacao das tabelas no banco

--- Models/Contexto.cs
+++ Models/Contexto.cs
@@ -5,5 +5,11 @@ namespace SistemaGestaoCantinasIgrejas.Models
     public class Contexto : DbContext
     {
         public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Igreja> igreja { get; set; }
+        public DbSet<Evento> evento { get; set; }
+        public DbSet<Participante> participante { get; set; }
+        public DbSet<Produto> produto { get; set; }
+        public DbSet<Venda> venda { get; set; }
     }
 }
