ca8a7664ef3a328b9f6c68f0082f891e38d28f07
GabrielGoncalvesDeOliveira <gabrielgo15r@gmail.com>
2022-10-09 15:43:58 -0300

    Implementacao das classes mapeando as entidades do banco

---
+++ Models/Evento.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SistemaGestaoCantinasIgrejas.Models
+{
+    [Table("Evento")]
+    public class Evento
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Id: ")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Campo descrição é obrigatório")]
+        [Display(Name = "Descrição: ")]
+        public string descricao { get; set; }
+
+        [Required(ErrorMessage = "Campo endereço é obrigatório")]
+        [Display(Name = "Endereço: ")]
+        public string endereco { get; set; }
+
+        [Required(ErrorMessage = "Campo horario é obrigatório")]
+        [Display(Name = "Horario: ")]
+        public string horario { get; set; }
+    }
+}
---
+++ Models/Venda.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Linq;
+
+namespace SistemaGestaoCantinasIgrejas.Models
+{
+    [Table("Venda")]
+    public class Venda
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Display(Name = "Id: ")]
+        public int id { get; set; }
+
+        [Required(ErrorMessage = "Campo id do participante é obrigatório")]
+        [Display(Name = "Id Participante: ")]
+        public int idParticipante { get; set; }
+
+        [Required(ErrorMessage = "Campo id do produto é obrigatório")]
+        [Display(Name = "Id Produto: ")]
+        public int idProduto { get; set; }
+
+        [Required(ErrorMessage = "Campo quantidade é obrigatório")]
+        [DisplayFormat(DataFormatString = "{0:N2}")]
+        [Display(Name = "Quantidade: ")]
+        public float quantidade { get; set; }
+
+        [Required(ErrorMessage = "Campo valor é obrigatório")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [Display(Name = "Valor: ")]
+        public float valor { get; set; }
+
+        [Display(Name = "Data: ")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        public DateTime data { get; set; }
+    }
+}
